f66a59d3426fe3c3e062f0f7f4e2e47ecbd68c8a
Rajdeep Malakar <Rajdeep@tgeeks.cf>
2021-10-26 16:48:04 +0530

    New Workflow

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,44 @@
 module.exports = {
-	pluginOptions: {
-		electronBuilder: {
-			nodeIntegration: true,
-			mainProcessFile: "main/background.js",
-			builderOptions: {
-				appId: "electron-store",
-				productName: "EA Store",
-				publish: [
-					{
-						provider: "github",
-						owner: "Electron-Store",
-						repo: "electron-app-store",
-					},
-				],
-				snap: {
-					title: "Electron Store",
-					confinement: "classic",
-					summary: "The Internet's App Store",
-					grade: "devel",
-				},
-			},
-		},
-	},
+    pluginOptions: {
+        electronBuilder: {
+            nodeIntegration: true,
+            mainProcessFile: "main/background.js",
+            builderOptions: {
+                appId: "electron-store",
+                productName: "Electron App Store",
+                publish: [{
+                    provider: "github",
+                    owner: "Electron-Store",
+                    repo: "electron-app-store",
+                }, ],
+                snap: {
+                    title: "Electron Store",
+                    confinement: "classic",
+                    summary: "The Internet's App Store",
+                    grade: "devel",
+                },
+                mac: {
+                    target: [{
+                        target: "dmg",
+                        arch: ["universal"]
+                    }]
+                },
+                linux: {
+                    target: [{
+                            target: "AppImage",
+                            arch: ['x64']
+                        },
+                        {
+                            target: "deb",
+                            arch: ['x64']
+                        },
+                        {
+                            target: "rpm",
+                            arch: ['x64']
+                        }
+                    ]
+                }
+            },
+        },
+    },
 };
